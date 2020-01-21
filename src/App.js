caa7131935092caef8ce33f45eb046a0edc78d5b
thisguycodez <guyton578@gmail.com>
2020-01-20 23:43:22 -0500

    some stretch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,23 @@ import BottomRow from "./BottomRow";
 
 function App() {
 
+
+  const touchDwn = team =>{
+    if(team.target.textContent = 'Home Touchdown'){
+      return   sethomeScore(homeScore + 7)
+    }
+
+    return setawayScore(awayScore + 7)
+}
+
+const kick = team =>{
+  if(team.target.textContent = 'Home Field Goal'){
+    return   sethomeScore(homeScore + 3)
+  }
+
+  return setawayScore(awayScore + 3)
+}
+
 {/*TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.*/
 }  const [homeScore, sethomeScore] = useState(32); 
 const [awayScore, setawayScore] = useState(32); 
@@ -36,9 +53,10 @@ return (
  function HomeTeamBtns(){
     return (
       <div className="homeButtons">
-     
+     {/*or I can have onClick={touchDwn}*/}
       <button className="homeButtons__touchdown" onClick={function(){sethomeScore(homeScore + 7)}}>Home Touchdown</button>
       <button className="homeButtons__fieldGoal" onClick={function(){sethomeScore(homeScore + 3)}}>Home Field Goal</button>
+     {/*or I can have onClick={kick}*/}
     </div>
     )
 }
@@ -48,8 +66,10 @@ return (
 function AwayTeamBtns(){
       return (
         <div className="awayButtons">
+     {/*or I can have onClick={touchDwn}*/}
           <button className="awayButtons__touchdown" onClick={function(){setawayScore(awayScore + 7)}}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={function(){setawayScore(awayScore + 3)}}>Away Field Goal</button>
+     {/*or I can have onClick={kick}*/}
         </div>
       )
 }
